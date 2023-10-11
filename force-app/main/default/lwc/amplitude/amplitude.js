76f9648989f8b2fba4eb39c61f882c61db5a735f
Morten Mikalsen <morten.mikalsen@hotmail.com>
2023-10-11 11:40:41 +0200

    Change to logEvent instead of tracking

--- force-app/main/default/lwc/amplitude/amplitude.js
+++ force-app/main/default/lwc/amplitude/amplitude.js
@@ -1,6 +1,6 @@
 export const trackAmplitudeEvent = (eventName, options = null) => {
     const amplitude = window.amplitude;
     if(amplitude) {
-        amplitude.track(eventName, options);
+        amplitude.logEvent(eventName, options);
     }
 }
