11302302cd52fa367b7a9bfd306035457f86b09c
eirikfladby <eirik.fladby@gmail.com>
2023-11-08 13:58:58 +0100

    add publish export func

--- force-app/main/default/lwc/amplitude/amplitude.js
+++ force-app/main/default/lwc/amplitude/amplitude.js
@@ -1,6 +1,19 @@
+import { createMessageContext, publish } from 'lightning/messageService';
+import AMPLITUDE_CHANNEL from '@salesforce/messageChannel/amplitude__c';
+
 export const trackAmplitudeEvent = (eventName, options = null) => {
     const amplitude = window.amplitude;
     if (amplitude) {
         amplitude.logEvent(eventName, options);
     }
+}
+
+let messageContext = createMessageContext();
+export function publishToAmplitude(eventType, properties, recordId) {
+    const message = {
+        eventType,
+        properties,
+        recordId
+    };
+    publish(messageContext, AMPLITUDE_CHANNEL, message);
 }
